16aee2c31e39658c8294ef800abe3c5657043e51
Seiryu Hidaka <e175726@ie.u-ryukyu.ac.jp>
2017-12-05 11:39:29 +0900

    ExDice finish

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
 import jp.ac.uryukyu.ie.ex3.pair13.Dice;
-
+import jp.ac.uryukyu.ie.ex3.pair13.ExDice;
 public class Main{
     public static void main(String[] args){
 	Dice dice = new Dice();
 	dice.play();
 	System.out.println(dice.getValue());
-
+	ExDice exdice = new ExDice(5,7);
+	for (int i = 1; i<11; i++){
+	exdice.play();
+	System.out.println("exdice"+i+"回目："+exdice.getValue());
+	}
     }
 }
+
